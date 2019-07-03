11b6e01813cf6ef9a1a4ead23ff103312af6a801
faimin <fuxianchao@gmail.com>
2019-07-03 18:45:06 +0800

    modify method

--- src/SuperClass.lua
+++ src/SuperClass.lua
@@ -6,61 +6,66 @@
 
 local SuperClass = {}
 SuperClass.__index = SuperClass
-local met = {}
-function met:__call( ... )
-    return SuperClass:new( ... )
+local mt = {}
+function mt:__call(...)
+    return SuperClass:new(...)
 end
-setmetatable(SuperClass, met)
+setmetatable(SuperClass, mt)
 ---下面这种写法有个弊端：第一个参数是table自身,所以改为上面的写法
 -- setmetatable(SuperClass, {
---     __call = function( ... )
---         for i, v in ipairs({ ... }) do
+--     __call = function(...)
+--         for i, v in ipairs({...}) do
 --             print("__call参数:", i, v)
 --         end
---         return SuperClass:new( ... )
+--         return SuperClass:new(...)
 --     end
 -- })
 
-function SuperClass:__call( ... )
-    for i, v in ipairs({ ... }) do
+function SuperClass:__call(...)
+    for i, v in ipairs({...}) do
         print("__call参数:", i, v)
     end
 
-    return self:new(...)
+    --函数也是属于table的一部分
+    return self:initialize(...)
 end
 
-function SuperClass:new( ... )
-    local subClass = {}
+function SuperClass:initialize(...)
+    local subClass = setmetatable({}, self)
     subClass.__index = subClass
-    setmetatable(subClass, self)
     subClass.super = self
     print("父类 = ", self, "子类 = ", subClass)
     return subClass
 end
 
+function SuperClass:new(...)
+    return self:initialize(...)
+end
+
 --测试代码
--- function SuperClass:setup()
---     print("父类setup方法")
---     return self
--- end
+--  function SuperClass:setup()
+--      print("父类setup方法 = ", self)
+--      return self
+--  end
 
--- local Object = SuperClass()
--- print("object = ", Object)
--- function Object:new( ... )
---     for i, v in ipairs({ ... }) do
---         print("子类参数:", i, v)
---     end
---     print("子new方法执行了")
---     return self
--- end
+--  local Object = SuperClass()
+--  print("object = ", Object)
+--  function Object:new(...)
+--      for i, v in ipairs({...}) do
+--          print("子类参数:", i, v)
+--      end
+--      print("子new方法执行了")
+--      return self
+--  end
 
--- local x = Object("1", "2", "3")
--- print("newObj = ", x)
--- print("子类未实现的方法：", x:setup())
+--  local x = Object("1", "2", "3")
+--  print("newObj = ", x)
+--  print("子类未实现的方法：", x:setup())
 
--- local xx = setmetatable({}, x)
--- xx.__index = xx
--- print("newnewObj = ", xx)
--- print("子类未实现的方法：", xx:setup())
+--  local xx = setmetatable({}, x)
+--  xx.__index = xx
+--  print("newnewObj = ", xx)
+--  print("孙类未实现的方法：", xx:setup())
+--  print("孙类的父类 = ", xx.super)
 
-return SuperClass()
+return SuperClass
