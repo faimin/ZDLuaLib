16066a61cd20b535c7bdbb90850d1bde49bc4773
Zero.D.Saber <fuxianchao@gmail.com>
2022-06-02 16:26:41 +0800

    support print table content

--- src/SuperClass.lua
+++ src/SuperClass.lua
@@ -4,7 +4,17 @@
 -----
 ----- Class Introduce: 面向对象(继承)
 
-local MT = {}
+local JSON = require("JSON")
+
+local MT = {
+    __tostring = function(t)
+        if JSON then
+            return JSON.encode(t)
+        else
+            return tostring(v)
+        end
+    end
+}
 MT.__index = MT
 MT.__call = function(self, ...)
     return self:new(...)
